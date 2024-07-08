8c5b9c9429bfbea719652819a0155cec2ce51437
anmoldeep02singh <130073831+anmoldeep02singh@users.noreply.github.com>
2024-07-08 18:33:37 +0000

    save changes in text.js

--- basic/text.js
+++ basic/text.js
@@ -1,1 +1,16 @@
-console.log ("ANMOLDEEP SINGH")
+// console.log ("ANMOLDEEP SINGH")
+const accountID = 142553
+let accountEmail = " anmoldeep02singh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"    // not use like this
+
+// accountID = 2          // not allowed
+accountEamil = "anmoldeep@.com"
+accountPassword = "2121212"
+accountCity = "Bengaluru"
+
+console.log( [accountID, accountEmail, accountPassword, accountCity])
+
+/* multiline comment */
+
+let accountState;  // variable declared but value not assigned // undefined
