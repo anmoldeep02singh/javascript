aa09709f093f28bc73f93f23be7c0011468bc976
anmoldeep02singh <130073831+anmoldeep02singh@users.noreply.github.com>
2024-07-08 19:04:35 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,35 @@
+" use strict "; // treat all the old js code as new version
+// alert (3+3) //we are using nodejs , not brower
+
+console.log( 3+3);
+console.log("Anmol");
+
+let name = "hitesh"      // string
+let age = 18           // number
+let isloggedIn = false   // boolean
+
+// premetive datatype
+/* number = 2 to power 53
+bigint =
+string = " "
+boolean = true/false
+null = standalone value / representation of empty value
+undefine = 
+symbole = unique */
+
+console.log(typeof age);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+let score = "33"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let score1 = "33abc"
+console.log(typeof score1);
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
